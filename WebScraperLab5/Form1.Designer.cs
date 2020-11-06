236c1a98342b37271320f8bbccee4cee14bcbf4e
Fredrik Moller <fredrik.moller@iths.se>
2020-11-06 14:47:28 +0100

    Fist draft

---
+++ WebScraperLab5/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WebScraperLab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_URLInsertion = new System.Windows.Forms.TextBox();
+            this.button_ExtractWeb = new System.Windows.Forms.Button();
+            this.button_SaveImages = new System.Windows.Forms.Button();
+            this.listBox_AllLoadedImages = new System.Windows.Forms.ListBox();
+            this.label_ImgCounter = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // textBox_URLInsertion
+            // 
+            this.textBox_URLInsertion.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_URLInsertion.Location = new System.Drawing.Point(60, 30);
+            this.textBox_URLInsertion.Name = "textBox_URLInsertion";
+            this.textBox_URLInsertion.Size = new System.Drawing.Size(598, 20);
+            this.textBox_URLInsertion.TabIndex = 0;
+            this.textBox_URLInsertion.Text = "Enter URL here";
+            this.textBox_URLInsertion.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_URLInsertion_KeyDown);
+            // 
+            // button_ExtractWeb
+            // 
+            this.button_ExtractWeb.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_ExtractWeb.Location = new System.Drawing.Point(689, 30);
+            this.button_ExtractWeb.Name = "button_ExtractWeb";
+            this.button_ExtractWeb.Size = new System.Drawing.Size(75, 23);
+            this.button_ExtractWeb.TabIndex = 2;
+            this.button_ExtractWeb.Text = "Extract";
+            this.button_ExtractWeb.UseVisualStyleBackColor = true;
+            this.button_ExtractWeb.Click += new System.EventHandler(this.button_ExtractWeb_Click);
+            // 
+            // button_SaveImages
+            // 
+            this.button_SaveImages.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_SaveImages.Location = new System.Drawing.Point(577, 391);
+            this.button_SaveImages.Name = "button_SaveImages";
+            this.button_SaveImages.Size = new System.Drawing.Size(75, 23);
+            this.button_SaveImages.TabIndex = 3;
+            this.button_SaveImages.Text = "Save";
+            this.button_SaveImages.UseVisualStyleBackColor = true;
+            this.button_SaveImages.Click += new System.EventHandler(this.button_SaveImages_Click);
+            // 
+            // listBox_AllLoadedImages
+            // 
+            this.listBox_AllLoadedImages.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox_AllLoadedImages.FormattingEnabled = true;
+            this.listBox_AllLoadedImages.HorizontalScrollbar = true;
+            this.listBox_AllLoadedImages.Location = new System.Drawing.Point(60, 81);
+            this.listBox_AllLoadedImages.Name = "listBox_AllLoadedImages";
+            this.listBox_AllLoadedImages.Size = new System.Drawing.Size(592, 303);
+            this.listBox_AllLoadedImages.TabIndex = 4;
+            // 
+            // label_ImgCounter
+            // 
+            this.label_ImgCounter.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label_ImgCounter.AutoSize = true;
+            this.label_ImgCounter.Location = new System.Drawing.Point(60, 391);
+            this.label_ImgCounter.Name = "label_ImgCounter";
+            this.label_ImgCounter.Size = new System.Drawing.Size(35, 13);
+            this.label_ImgCounter.TabIndex = 5;
+            this.label_ImgCounter.Text = "label1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(186, 391);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(373, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 473);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label_ImgCounter);
+            this.Controls.Add(this.listBox_AllLoadedImages);
+            this.Controls.Add(this.button_SaveImages);
+            this.Controls.Add(this.button_ExtractWeb);
+            this.Controls.Add(this.textBox_URLInsertion);
+            this.MinimumSize = new System.Drawing.Size(850, 512);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_URLInsertion;
+        private System.Windows.Forms.Button button_ExtractWeb;
+        private System.Windows.Forms.Button button_SaveImages;
+        private System.Windows.Forms.ListBox listBox_AllLoadedImages;
+        private System.Windows.Forms.Label label_ImgCounter;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ WebScraperLab5/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebScraperLab5
+{
+    public partial class Form1 : Form
+    {
+        private List<string> urlStrings = new List<string>();
+        private HttpClient client = new HttpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void ExtractUrls()
+        {
+            button_SaveImages.Enabled = true;
+            urlStrings.Clear();
+            listBox_AllLoadedImages.Items.Clear();
+
+            if(textBox_URLInsertion.Text == string.Empty)
+            {
+                MessageBox.Show("enter a webadress", "invalid Input");
+                return;
+            }
+
+            if (!textBox_URLInsertion.Text.Contains("http://"))
+            {
+                textBox_URLInsertion.Text = $"http://{textBox_URLInsertion.Text}";
+            }
+
+            Task<string> website = client.GetStringAsync(textBox_URLInsertion.Text);
+
+            string regEx = @"(?<=src="").*(?="" )";
+
+            MatchCollection matchCollection = Regex.Matches(website.Result, regEx);
+
+            foreach (Match img in matchCollection)
+            {
+                if (img.ToString().Contains(".png") || img.ToString().Contains(".jpeg") || img.ToString().Contains(".jpg") ||
+                    img.ToString().Contains(".bmp") || img.ToString().Contains(".gif"))
+                {
+                    if (img.ToString().StartsWith("http://www.gp.se"))
+                    {
+                        urlStrings.Add(img.ToString());
+                    }
+                    if (!img.ToString().StartsWith("https"))
+                    {
+                        string newUrl = $"http://www.gp.se{img}";
+                        urlStrings.Add(newUrl);
+                    }
+                }
+            }
+            foreach (string url in urlStrings)
+            {
+                listBox_AllLoadedImages.Items.Add(url);
+            }
+
+            label_ImgCounter.Visible = true;
+            label_ImgCounter.Text = $"Number of images: {listBox_AllLoadedImages.Items.Count}";
+        }
+
+        private void button_ExtractWeb_Click(object sender, EventArgs e)
+        {
+            ExtractUrls();
+        }
+
+        private void textBox_URLInsertion_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) { ExtractUrls(); e.SuppressKeyPress = true; }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label_ImgCounter.Visible = false;
+            button_SaveImages.Enabled = false;
+        }
+        private async void button_SaveImages_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    int numPic = 0;
+
+                    List<Task<byte[]>> downloadPic = new List<Task<byte[]>>();
+                    foreach (var url in urlStrings)
+                    {
+                        downloadPic.Add(client.GetByteArrayAsync(url));
+                    }
+
+                    while (downloadPic.Count > 0)
+                    {
+                        int bar = 0;
+
+                        foreach (var url in listBox_AllLoadedImages.Items)
+                        {
+                            Task<byte[]> completedTask = await Task.WhenAny(downloadPic);
+
+                            numPic++;
+
+                            if (url.ToString().Contains(".jpg") || url.ToString().Contains(".jpeg"))
+                            {
+                                FileStream fsjpg = new FileStream($@"{dialog.SelectedPath}\image{numPic}.jpg", FileMode.Create);
+                                await fsjpg.WriteAsync(completedTask.Result, 0, completedTask.Result.Length);
+                            }
+                            if (url.ToString().Contains(".bmp"))
+                            {
+                                FileStream fsbmp = new FileStream($@"{dialog.SelectedPath}\image{numPic}.bmp", FileMode.Create);
+                                await fsbmp.WriteAsync(completedTask.Result, 0, completedTask.Result.Length);
+                            }
+                            if (url.ToString().Contains(".png"))
+                            {
+                                FileStream fspng = new FileStream($@"{dialog.SelectedPath}\image{numPic}.png", FileMode.Create);
+                                await fspng.WriteAsync(completedTask.Result, 0, completedTask.Result.Length);
+                            }
+                            if (url.ToString().Contains(".gif"))
+                            {
+                                FileStream fsgif = new FileStream($@"{dialog.SelectedPath}\image{numPic}.gif", FileMode.Create);
+                                await fsgif.WriteAsync(completedTask.Result, 0, completedTask.Result.Length);
+                            }
+
+                            downloadPic.Remove(completedTask);
+                            progressBar1.Maximum = listBox_AllLoadedImages.Items.Count;
+                            bar++;
+                            progressBar1.Value = bar;
+                        }
+                    }
+                    MessageBox.Show($"{listBox_AllLoadedImages.Items.Count} images downloaded", "Download Complete");
+                    progressBar1.Value = 0;
+                    listBox_AllLoadedImages.Items.Clear();
+                    label_ImgCounter.Visible = false;
+                    label_ImgCounter.Text = $"Number of images: {listBox_AllLoadedImages.Items.Count}";
+                }
+            }
+        }
+    }
+}
